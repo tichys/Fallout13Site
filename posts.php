73d56c7c57ee13cc23e0e42351b4cb5512c6078c
ErwinsExpertise <44278151+ErwinsExpertise@users.noreply.github.com>
2019-07-17 15:55:49 -0500

    update

--- posts.php
+++ posts.php
@@ -11,7 +11,7 @@ $result = $mysqli->query($sql);
 
 if ($result->num_rows > 0) {
     while($row = $result->fetch_assoc()) {
-        echo "<div class="card"> <p> Posted By: " . $row["author"]. "   Date: ". $row["dateposted"] ."</p>  <div class="card-body"> <p>" . $row["post"]. "</p></div></div><hr>";
+        echo '<div class="card"> <p> Posted By: ' . $row["author"]. '   Date: ' . $row["dateposted"] .'</p>  <div class="card-body"> <p>' . $row["post"]. '</p></div></div><hr>';
     }
 } else {
     echo "0 results";
