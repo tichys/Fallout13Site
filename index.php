fe33d8d92699fd047bc47214ebafe05b4854abb9
ErwinsExpertise <44278151+ErwinsExpertise@users.noreply.github.com>
2019-07-17 17:28:36 -0500

    update

--- index.php
+++ index.php
@@ -35,6 +35,8 @@
     <div class="row">
   <div class="col-sm-4" id="pipboy">
     <img src="images/pip.png" data-spy="affix" data-offset="15">
+    <br>
+    <iframe src="https://discordapp.com/widget?id=583059885881819145&theme=dark" width="350" height="500" allowtransparency="true" frameborder="0"></iframe>
   </div>
   <div class="col-sm-8">
     <div class="jumbotron bg-dark" style="color: #1bff1f;">
