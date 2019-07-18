439220b61d40fd5fda87c7893d1183fc737ad9dd
ErwinsExpertise <44278151+ErwinsExpertise@users.noreply.github.com>
2019-07-17 19:28:37 -0500

    word wrap

--- posts.php
+++ posts.php
@@ -11,7 +11,7 @@ $result = $mysqli->query($sql);
 
 if ($result->num_rows > 0) {
     while($row = $result->fetch_assoc()) {
-        echo '<div class="card text-left bg-secondary" style="color: #1bff1f; padding-left: 10px; padding-right: 10px; padding-bottom: 15px; border-radius: 5px;">Posted By: ' . $row["author"]. '<div class="text-right">Date: ' . date("d-m-y",$row["dateposted"]) .'</div><hr> <div class="card-body text-left" style="background-color: black; border-radius: 15px; overflow-wrap:break-word;"> <p>' . $row["post"]. '</div></div><hr>';
+        echo '<div class="card text-left bg-secondary" style="color: #1bff1f; padding-left: 10px; padding-right: 10px; padding-bottom: 15px; border-radius: 5px; word-wrap: break-word;">Posted By: ' . $row["author"]. '<div class="text-right">Date: ' . date("d-m-y",$row["dateposted"]) .'</div><hr> <div class="card-body text-left" style="background-color: black; border-radius: 15px; word-wrap: break-word;"> <p>' . $row["post"]. '</div></div><hr>';
     }
 } else {
     echo "0 results";
