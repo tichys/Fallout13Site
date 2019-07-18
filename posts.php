b0fd2ac4016be06e1f162ec02c64239e74254e79
ErwinsExpertise <44278151+ErwinsExpertise@users.noreply.github.com>
2019-07-17 19:33:37 -0500

    fsd

--- posts.php
+++ posts.php
@@ -11,7 +11,7 @@ $result = $mysqli->query($sql);
 
 if ($result->num_rows > 0) {
     while($row = $result->fetch_assoc()) {
-        echo '<div class="card text-left bg-secondary" style="max-width: 100%; color: #1bff1f; padding-left: 10px; padding-right: 10px; padding-bottom: 15px; border-radius: 5px; word-wrap: break-word;">Posted By: ' . $row["author"]. '<div class="text-right">Date: ' . date("d-m-y",$row["dateposted"]) .'</div><hr> <div class="card-body text-left" style="max-width: 100%; background-color: black; border-radius: 15px; word-wrap: break-word;"> <p>' . $row["post"]. '</div></div><hr>';
+        echo '<div class="card text-left bg-secondary" style="width: 50px; color: #1bff1f; padding-left: 10px; padding-right: 10px; padding-bottom: 15px; border-radius: 5px; word-wrap: break-word;">Posted By: ' . $row["author"]. '<div class="text-right">Date: ' . date("d-m-y",$row["dateposted"]) .'</div><hr> <div class="card-body text-left" style="width: 50px; background-color: black; border-radius: 15px; word-wrap: break-word;"> <p>' . $row["post"]. '</div></div><hr>';
     }
 } else {
     echo "0 results";
