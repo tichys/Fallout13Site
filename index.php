c80ad5599de5011e94112755b78c0cf6ae935efe
ErwinsExpertise <44278151+ErwinsExpertise@users.noreply.github.com>
2019-07-19 16:13:23 -0500

    yeah

--- index.php
+++ index.php
@@ -36,12 +36,13 @@
   </head>
   <div class="container bg-dark">
 <body data-spy="scroll" data-target="#pipboy" data-offset="0">
+    <div class="row">
   <div class="col-sm-4" id="pipboy">
     <img src="images/pip.png" data-spy="affix" data-offset="0">
     <br><br><br>
     <iframe src="https://discordapp.com/widget?id=583059885881819145&theme=dark" width="350" height="500" allowtransparency="true" frameborder="0"></iframe>
   </div>
-  <div class="col-sm-8">
+  <div class="col-sm-6">
     <div class="jumbotron bg-dark" style="color: #1bff1f;">
       <div class="alert alert-success" role="alert">
   <h1>News</h1>
@@ -51,7 +52,7 @@
       <?php include('posts.php') ?>
     </div>
   </div>
-
+</div>
   </body>
     </div>
 
