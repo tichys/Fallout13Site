4d069d888a0062a2cfbf265f3cf3df368ad1fe53
ErwinsExpertise <44278151+ErwinsExpertise@users.noreply.github.com>
2019-07-19 12:20:57 -0500

    updated header

--- events.php
+++ events.php
@@ -1,8 +1,37 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
 <head>
-  <include src="./header.html"></include>
-</head>
+  <meta charset="utf-8">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+  <link href="https://fonts.googleapis.com/css?family=Share+Tech+Mono&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="css/master.css">
+  <title>Fallout 13: A New Dawn</title>
+  <nav class="navbar navbar-expand-lg navbar-secondary shadow sticky-top" style="padding: 1rem; position: relative; background: linear-gradient(to bottom, #1d6029, #1d5628, black); padding: 10px;">
+    <div class="container">
+      <a class="navbar-brand" href="#">Fallout 13: A New Dawn</a>
+        <ul class="nav nav-tabs ml-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+        </li>
+      <li class="nav-item">
+                <a class="nav-link" href="faq.html">FAQ</a>
+      </li>
+      <li class="nav-item">
+            <a class="nav-link" href="about.html">About Us</a>
+      </li>
+      <li class="nav-item">
+            <a class="nav-link" href="/wiki">Wiki</a>
+      </li>
+      <li class="nav-item">
+            <a class="nav-link" href="events.php">Events</a>
+      </li>
+    </ul>
+    </div>
+  </nav>
+  </head>
   <div class="container bg-dark">
   <body>
     <div class="jumbotron bg-dark" style="color: #1bff1f;">
